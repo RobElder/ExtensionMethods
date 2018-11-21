e62187d6ed2bf20d71923fafe7dd638b39ffd13b
Robert Elder <robert.elder@gmail.com>
2018-11-20 21:22:17 -0500

    Init Solution

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            string s = "Hello world!";
+            string hash = s.ComputeHash(Hasher.eHashType.RIPEMD160);
+
+            Console.WriteLine(hash);
+
+            //var myEnum = "Pending".ToEnum<Status>();
+
+            var left = "Faraz Masood Khan".Left(100000);// works;
+            var right = "CoreSystem Library".Right(10);
+            var result = "CoreSystem".In("CoreSystem", "Library");
+
+            var s1 = "aaaaaaaabbbbccccddddeeeeeeeeeeee".FormatWithMask("Hello ########-#A###-####-####-############ Oww");
+            var s2 = "abc".FormatWithMask("###-#");
+            var s3 = "".FormatWithMask("Hello ########-#A###-####-####-############ Oww");
+
+            string[] names = new string[] { "C#", "Java" };
+            names.ForEach(i => Console.WriteLine(i));
+
+            IEnumerable<int> namesLen = names.ForEach(i => i.Length);
+            namesLen.ForEach(i => Console.WriteLine(i));
+
+       
+
+        MyEnum a = Enum<MyEnum>.Parse("foo"); //Returns MyEnum.Foo
+        MyEnum b = Enum<MyEnum>.Parse("foo", false); //Fails as the parse is case sensitive
+
+        //Try Parse
+        MyEnum c = MyEnum.Foo;
+ 
+        if(Enum<MyEnum>.TryParse("Fi", out c) == false)
+        {
+            //throw an error
+        }
+
+        
+
+        string value = "abc";
+        bool isnumeric = value.IsNumeric();// Will return false;
+        value = "11";
+        isnumeric = value.IsNumeric();// Will return true;
+        }
+    }
+
+    //Enum
+    public enum MyEnum
+    {
+        Foo,
+        Bar,
+    }
+
+    public static class Enum<T>
+    {
+        public static T Parse(string value)
+        {
+            return Enum<T>.Parse(value, true);
+        }
+
+        public static T Parse(string value, bool ignoreCase)
+        {
+            return (T)Enum.Parse(typeof(T), value, ignoreCase);
+        }
+
+        public static bool TryParse(string value, out T returnedValue)
+        {
+            return Enum<T>.TryParse(value, true, out returnedValue);
+        }
+
+        public static bool TryParse(string value, bool ignoreCase, out T returnedValue)
+        {
+            try
+            {
+                returnedValue = (T)Enum.Parse(typeof(T), value, ignoreCase);
+                return true;
+            }
+            catch
+            {
+                returnedValue = default(T);
+                return false;
+            }
+        }
+    }
+
+    public static class HttpEx
+    {
+        public static string HtmlEncode(this string data)
+        {
+            return HttpUtility.HtmlEncode(data);
+        }
+
+        public static string HtmlDecode(this string data)
+        {
+            return HttpUtility.HtmlDecode(data);
+        }
+
+        public static NameValueCollection ParseQueryString(this string query)
+        {
+            return HttpUtility.ParseQueryString(query);
+        }
+
+        public static string UrlEncode(this string url)
+        {
+            return HttpUtility.UrlEncode(url);
+        }
+
+        public static string UrlDecode(this string url)
+        {
+            return HttpUtility.UrlDecode(url);
+        }
+
+        public static string UrlPathEncode(this string url)
+        {
+            return HttpUtility.UrlPathEncode(url);
+        }
+    }
+
+
+    public static class Extensions
+    {
+        public static bool IsDate(this string input)
+        {
+            if (!string.IsNullOrEmpty(input))
+            {
+                DateTime dt;
+                return (DateTime.TryParse(input, out dt));
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsNotNullOrEmpty<T>(this System.Collections.Generic.IEnumerable<T> source)
+        {
+            return source != null && source.Any();
+        }
+
+        public static bool IsNumeric(this string theValue)
+        {
+            long retNum;
+            return long.TryParse(theValue, System.Globalization.NumberStyles.Integer, System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+        }
+
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> array, Action<T> act)
+        {
+            foreach (var i in array)
+                act(i);
+            return array;
+        }
+
+        public static IEnumerable<T> ForEach<T>(this IEnumerable arr, Action<T> act)
+        {
+            return arr.Cast<T>().ForEach<T>(act);
+        }
+
+        public static IEnumerable<RT> ForEach<T, RT>(this IEnumerable<T> array, Func<T, RT> func)
+        {
+            var list = new List<RT>();
+            foreach (var i in array)
+            {
+                var obj = func(i);
+                if (obj != null)
+                    list.Add(obj);
+            }
+            return list;
+        }
+
+        public static DataTable ToDataTable<T>(this IEnumerable<T> varlist)
+        {
+            DataTable dtReturn = new DataTable();
+
+            // column names 
+            PropertyInfo[] oProps = null;
+
+            if (varlist == null) return dtReturn;
+
+            foreach (T rec in varlist)
+            {
+                // Use reflection to get property names, to create table, Only first time, others will follow 
+                if (oProps == null)
+                {
+                    oProps = ((Type)rec.GetType()).GetProperties();
+                    foreach (PropertyInfo pi in oProps)
+                    {
+                        Type colType = pi.PropertyType;
+
+                        if ((colType.IsGenericType) && (colType.GetGenericTypeDefinition() == typeof(Nullable<>)))
+                        {
+                            colType = colType.GetGenericArguments()[0];
+                        }
+
+                        dtReturn.Columns.Add(new DataColumn(pi.Name, colType));
+                    }
+                }
+
+                DataRow dr = dtReturn.NewRow();
+
+                foreach (PropertyInfo pi in oProps)
+                {
+                    dr[pi.Name] = pi.GetValue(rec, null) == null ? DBNull.Value : pi.GetValue
+                        (rec, null);
+                }
+
+                dtReturn.Rows.Add(dr);
+            }
+            return dtReturn;
+        }
+    }
+
+    public static class Hasher
+    {
+        /// <summary>
+        /// Supported hash algorithms
+        /// </summary>
+        public enum eHashType
+        {
+            HMAC, HMACMD5, HMACSHA1, HMACSHA256, HMACSHA384, HMACSHA512,
+            MACTripleDES, MD5, RIPEMD160, SHA1, SHA256, SHA384, SHA512
+        }
+
+        private static byte[] GetHash(string input, eHashType hash)
+        {
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+
+            switch (hash)
+            {
+                case eHashType.HMAC:
+                    return HMAC.Create().ComputeHash(inputBytes);
+
+                case eHashType.HMACMD5:
+                    return HMACMD5.Create().ComputeHash(inputBytes);
+
+                case eHashType.HMACSHA1:
+                    return HMACSHA1.Create().ComputeHash(inputBytes);
+
+                case eHashType.HMACSHA256:
+                    return HMACSHA256.Create().ComputeHash(inputBytes);
+
+                case eHashType.HMACSHA384:
+                    return HMACSHA384.Create().ComputeHash(inputBytes);
+
+                case eHashType.HMACSHA512:
+                    return HMACSHA512.Create().ComputeHash(inputBytes);
+
+                case eHashType.MACTripleDES:
+                    return MACTripleDES.Create().ComputeHash(inputBytes);
+
+                case eHashType.MD5:
+                    return MD5.Create().ComputeHash(inputBytes);
+
+                case eHashType.RIPEMD160:
+                    return RIPEMD160.Create().ComputeHash(inputBytes);
+
+                case eHashType.SHA1:
+                    return SHA1.Create().ComputeHash(inputBytes);
+
+                case eHashType.SHA256:
+                    return SHA256.Create().ComputeHash(inputBytes);
+
+                case eHashType.SHA384:
+                    return SHA384.Create().ComputeHash(inputBytes);
+
+                case eHashType.SHA512:
+                    return SHA512.Create().ComputeHash(inputBytes);
+
+                default:
+                    return inputBytes;
+            }
+        }
+
+        /// <summary>
+        /// Computes the hash of the string using a specified hash algorithm
+        /// </summary>
+        /// <param name="input">The string to hash</param>
+        /// <param name="hashType">The hash algorithm to use</param>
+        /// <returns>The resulting hash or an empty string on error</returns>
+        public static string ComputeHash(this string input, eHashType hashType)
+        {
+            try
+            {
+                byte[] hash = GetHash(input, hashType);
+                StringBuilder ret = new StringBuilder();
+
+                for (int i = 0; i < hash.Length; i++)
+                    ret.Append(hash[i].ToString("x2"));
+
+                return ret.ToString();
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+
+    /// <summary>
+    /// This class contain extension functions for string objects
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Checks string object's value to array of string values
+        /// </summary>        
+        /// <param name="stringValues">Array of string values to compare</param>
+        /// <returns>Return true if any string value matches</returns>
+        public static bool In(this string value, params string[] stringValues)
+        {
+            foreach (string otherValue in stringValues)
+                if (string.Compare(value, otherValue) == 0)
+                    return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Converts string to enum object
+        /// </summary>
+        /// <typeparam name="T">Type of enum</typeparam>
+        /// <param name="value">String value to convert</param>
+        /// <returns>Returns enum object</returns>
+        public static T ToEnum<T>(this string value)
+            where T : struct
+        {
+            return (T)System.Enum.Parse(typeof(T), value, true);
+        }
+
+        /// <summary>
+        /// Returns characters from right of specified length
+        /// </summary>
+        /// <param name="value">String value</param>
+        /// <param name="length">Max number of charaters to return</param>
+        /// <returns>Returns string from right</returns>
+        public static string Right(this string value, int length)
+        {
+            return value != null && value.Length > length ? value.Substring(value.Length - length) : value;
+        }
+
+        /// <summary>
+        /// Returns characters from left of specified length
+        /// </summary>
+        /// <param name="value">String value</param>
+        /// <param name="length">Max number of charaters to return</param>
+        /// <returns>Returns string from left</returns>
+        public static string Left(this string value, int length)
+        {
+            return value != null && value.Length > length ? value.Substring(0, length) : value;
+        }
+
+        /// <summary>
+        ///  Replaces the format item in a specified System.String with the text equivalent
+        ///  of the value of a specified System.Object instance.
+        /// </summary>
+        /// <param name="value">A composite format string</param>
+        /// <param name="arg0">An System.Object to format</param>
+        /// <returns>A copy of format in which the first format item has been replaced by the
+        /// System.String equivalent of arg0</returns>
+        public static string Format(this string value, object arg0)
+        {
+            return string.Format(value, arg0);
+        }
+
+        /// <summary>
+        ///  Replaces the format item in a specified System.String with the text equivalent
+        ///  of the value of a specified System.Object instance.
+        /// </summary>
+        /// <param name="value">A composite format string</param>
+        /// <param name="args">An System.Object array containing zero or more objects to format.</param>
+        /// <returns>A copy of format in which the format items have been replaced by the System.String
+        /// equivalent of the corresponding instances of System.Object in args.</returns>
+        public static string Format(this string value, params object[] args)
+        {
+            return string.Format(value, args);
+        }
+
+        public static bool IsNullOrEmpty(this string s) => s == null || string.IsNullOrEmpty(s);
+
+        // <summary>
+        /// Formats the string according to the specified mask
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <param name="mask">The mask for formatting. Like "A##-##-T-###Z"</param>
+        /// <returns>The formatted string</returns>
+        public static string FormatWithMask(this string input, string mask)
+        {
+            if (input.IsNullOrEmpty()) return input;
+            var output = string.Empty;
+            var index = 0;
+            foreach (var m in mask)
+            {
+                if (m == '#')
+                {
+                    if (index < input.Length)
+                    {
+                        output += input[index];
+                        index++;
+                    }
+                }
+                else
+                    output += m;
+            }
+            return output;
+        }
+    }
+
+    public static class ScottGuExtensions
+    {
+        public static bool IsValidEmailAddress(this string s)
+        {
+            Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            return regex.IsMatch(s);
+        }
+    }
+}
